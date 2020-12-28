a9aaee1d4b532ce552cd15c291f32ff43dc704f8
Sandeep Singhal <sandeepsinghal@gmail.com>
2020-12-28 15:19:13 -0500

    functions in the component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
   render() {
     return (
-      <React.Fragment>
-        <h1>Hello World again</h1>
-        <button>Increment</button>{" "}
-      </React.Fragment>
+      <div>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count == 0 ? <h1>Zero</h1> : <h1>Non Zero</h1>;
   }
 }
 
