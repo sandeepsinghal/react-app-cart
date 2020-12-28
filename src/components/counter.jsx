91a9968f48913399e8569e0a5727b4cc9759699f
Sandeep Singhal <sandeepsinghal@gmail.com>
2020-12-28 15:12:04 -0500

    using a react fragment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 class Counter extends Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
         <h1>Hello World again</h1>
         <button>Increment</button>{" "}
-      </div>
+      </React.Fragment>
     );
   }
 }
