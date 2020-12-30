8a6f11c00b318081aa1e726c7558d77958f13d2a
Sandeep Singhal <sandeepsinghal@gmail.com>
2020-12-30 17:28:41 -0500

    consolidating data into a "single source of truth"

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,6 +11,24 @@ class Counters extends Component {
     ],
   };
 
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    console.log(this.state.counters[index]);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    console.log("here");
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   handleDelete = (counterId) => {
     console.log("Event handler called : ", counterId);
     const counters = this.state.counters.filter((c) => c.id !== counterId);
@@ -20,10 +38,17 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
           />
         ))}
