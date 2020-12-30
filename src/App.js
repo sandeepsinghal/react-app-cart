3912f7b4bbcb468da2da62f0a2ab896971fb831d
Sandeep Singhal <sandeepsinghal@gmail.com>
2020-12-30 17:56:07 -0500

    lifting the state to parent level class

--- src/App.js
+++ src/App.js
@@ -6,12 +6,50 @@ import Counters from './components/counters';
 
 
 class App extends Component{
+    state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    console.log(this.state.counters[index]);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    console.log("here");
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    console.log("Event handler called : ", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render(){
     return (
       <React.Fragment>
         <NavBar/>
         <main className="container">
-          <Counters />
+          <Counters 
+            counters={this.state.counters}
+            onReset={this.handleReset} 
+            onIncrement={this.handleIncrement}
+            onDelete = {this.handleDelete}
+           />
         </main>
       </React.Fragment>
     );
